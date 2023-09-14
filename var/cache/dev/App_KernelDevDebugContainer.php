6ec1ab1d1e6df59908446b4ae7901f814be22889
Romain.jhr <ramjoh@MacBook-Pro-de-Romain.local>
2023-09-14 10:30:02 +0200

    base.twig

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,6 +16,6 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerFN1xaTf\App_KernelDevDebugContainer([
     'container.build_hash' => 'FN1xaTf',
-    'container.build_id' => '9d9bfc1e',
-    'container.build_time' => 1694635382,
+    'container.build_id' => 'bbb52785',
+    'container.build_time' => 1694639152,
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerFN1xaTf');
--- var/cache/dev/twig/a1/a1594d5a44582bea6669ea32c5bb730e.php
+++ var/cache/dev/twig/a1/a1594d5a44582bea6669ea32c5bb730e.php
@@ -29,8 +29,9 @@ class __TwigTemplate_3994b5ff96d42bc3ac661ea5acb6e2cd extends Template
         $this->blocks = [
             'title' => [$this, 'block_title'],
             'stylesheets' => [$this, 'block_stylesheets'],
-            'javascripts' => [$this, 'block_javascripts'],
             'body' => [$this, 'block_body'],
+            'content' => [$this, 'block_content'],
+            'javascripts' => [$this, 'block_javascripts'],
         ];
     }
 
@@ -52,22 +53,20 @@ class __TwigTemplate_3994b5ff96d42bc3ac661ea5acb6e2cd extends Template
         // line 5
         $this->displayBlock('title', $context, $blocks);
         echo "</title>
-        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text></svg>\">
+        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 128 128'><text y='1.2em' font-size='96'>⚫️</text></svg>\">
         ";
         // line 7
         $this->displayBlock('stylesheets', $context, $blocks);
-        // line 9
-        echo "
-        ";
-        // line 10
-        $this->displayBlock('javascripts', $context, $blocks);
-        // line 12
+        // line 19
         echo "    </head>
-    <body>
+        <body id=\"home\">
         ";
-        // line 14
+        // line 21
         $this->displayBlock('body', $context, $blocks);
-        // line 15
+        // line 59
+        echo "        ";
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 67
         echo "    </body>
 </html>
 ";
@@ -89,7 +88,7 @@ class __TwigTemplate_3994b5ff96d42bc3ac661ea5acb6e2cd extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
 
-        echo "Welcome!";
+        echo "Tricount";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
@@ -109,27 +108,34 @@ class __TwigTemplate_3994b5ff96d42bc3ac661ea5acb6e2cd extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
 
         // line 8
-        echo "        ";
-        
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
-
-        
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
-
-    }
-
-    // line 10
-    public function block_javascripts($context, array $blocks = [])
-    {
-        $macros = $this->macros;
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
-
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
-
+        echo "            <link rel=\"stylesheet\" href=\"";
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/css/bootstrap.min.css"), "html", null, true);
+        echo "\">
+            <link rel=\"stylesheet\" href=\"";
+        // line 9
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/css/ionicons.min.css"), "html", null, true);
+        echo "\">
+            <link rel=\"stylesheet\" href=\"";
+        // line 10
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/css/owl.carousel.css"), "html", null, true);
+        echo "\">
+            <link rel=\"stylesheet\" href=\"";
         // line 11
-        echo "        ";
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/css/owl.theme.css"), "html", null, true);
+        echo "\">
+            <link rel=\"stylesheet\" href=\"";
+        // line 12
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/css/animate.css"), "html", null, true);
+        echo "\">
+            <link rel=\"stylesheet\" href=\"";
+        // line 13
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/css/style.css"), "html", null, true);
+        echo "\">
+
+            <!-- fonts -->
+            <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic|Roboto+Condensed:300italic,400italic,700italic,400,300,700|Oxygen:400,300,700' rel='stylesheet'>
+            <script src=\"https://kit.fontawesome.com/9b628f0366.js\" crossorigin=\"anonymous\"></script>
+        ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
@@ -138,7 +144,7 @@ class __TwigTemplate_3994b5ff96d42bc3ac661ea5acb6e2cd extends Template
 
     }
 
-    // line 14
+    // line 21
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -148,6 +154,106 @@ class __TwigTemplate_3994b5ff96d42bc3ac661ea5acb6e2cd extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
+        // line 22
+        echo "            <!-- ****************************** Preloader ************************** -->
+
+            <div id=\"preloader\"></div>
+
+            <!-- ****************************** Sidebar ************************** -->
+
+            <nav id=\"sidebar-wrapper\">
+                <a id=\"menu-close\" href=\"#\" class=\"close-btn toggle\"><i class=\"ion-ios-close-empty\"></i></a>
+                <ul class=\"sidebar-nav\">
+                    <li><a href=\"/user/\"><i class=\"fa-solid fa-right-to-bracket\"></i></a></li>
+                    <li><a href=\"#home\">Home</a></li>
+                    <li><a href=\"#features\">Features</a></li>
+                    <li><a href=\"#gallery\">Gallery</a></li>
+                    <li><a href=\"#team\">Development Team</a></li>
+                    <li><a href=\"#contact\">Contact us</a></li>
+                </ul>
+            </nav>
+
+            <!-- ****************************** Header ************************** -->
+
+            <header class=\"sticky\" id=\"header\">
+                <section class=\"container\">
+                    <section class=\"row\" id=\"logo_menu\">
+                        <section class=\"col-xs-8\"><a class=\"logo\" href=\"\">T R I C O U N T</a></section>
+                        <section class=\"col-xs-4\"><a id=\"menu-toggle\" href=\"#\" class=\"toggle wow rotateIn\" data-wow-delay=\"1s\"><i class=\"ion-navicon\"></i></a></section>
+                    </section>
+                </section>
+            </header>
+
+            <!-- ****************************** Banner ************************** -->
+            <div>
+                ";
+        // line 53
+        $this->displayBlock('content', $context, $blocks);
+        // line 56
+        echo "            </div>
+            
+        ";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 53
+    public function block_content($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "content"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "content"));
+
+        // line 54
+        echo "                    <!-- Contenu spécifique de chaque page -->
+                ";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 59
+    public function block_javascripts($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
+
+        // line 60
+        echo "            <script src=\"";
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/js/jquery-2.1.3.min.js"), "html", null, true);
+        echo "\"></script>
+            <script src=\"";
+        // line 61
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/js/bootstrap.min.js"), "html", null, true);
+        echo "\"></script>
+            <script src=\"";
+        // line 62
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/js/wow.min.js"), "html", null, true);
+        echo "\"></script>
+            <script src=\"";
+        // line 63
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/js/owl.carousel.js"), "html", null, true);
+        echo "\"></script>
+            <script src=\"";
+        // line 64
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("assets/js/script.js"), "html", null, true);
+        echo "\"></script>
+            
+        ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
@@ -163,7 +269,7 @@ class __TwigTemplate_3994b5ff96d42bc3ac661ea5acb6e2cd extends Template
 
     public function getDebugInfo()
     {
-        return array (  142 => 14,  132 => 11,  122 => 10,  112 => 8,  102 => 7,  83 => 5,  71 => 15,  69 => 14,  65 => 12,  63 => 10,  60 => 9,  58 => 7,  53 => 5,  47 => 1,);
+        return array (  253 => 64,  249 => 63,  245 => 62,  241 => 61,  236 => 60,  226 => 59,  215 => 54,  205 => 53,  193 => 56,  191 => 53,  158 => 22,  148 => 21,  132 => 13,  128 => 12,  124 => 11,  120 => 10,  116 => 9,  111 => 8,  101 => 7,  82 => 5,  70 => 67,  67 => 59,  65 => 21,  61 => 19,  59 => 7,  54 => 5,  48 => 1,);
     }
 
     public function getSourceContext()
@@ -172,16 +278,68 @@ class __TwigTemplate_3994b5ff96d42bc3ac661ea5acb6e2cd extends Template
 <html>
     <head>
         <meta charset=\"UTF-8\">
-        <title>{% block title %}Welcome!{% endblock %}</title>
-        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text></svg>\">
+        <title>{% block title %}Tricount{% endblock %}</title>
+        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 128 128'><text y='1.2em' font-size='96'>⚫️</text></svg>\">
         {% block stylesheets %}
-        {% endblock %}
+            <link rel=\"stylesheet\" href=\"{{ asset(\"assets/css/bootstrap.min.css\") }}\">
+            <link rel=\"stylesheet\" href=\"{{ asset(\"assets/css/ionicons.min.css\") }}\">
+            <link rel=\"stylesheet\" href=\"{{ asset(\"assets/css/owl.carousel.css\") }}\">
+            <link rel=\"stylesheet\" href=\"{{ asset(\"assets/css/owl.theme.css\") }}\">
+            <link rel=\"stylesheet\" href=\"{{ asset(\"assets/css/animate.css\") }}\">
+            <link rel=\"stylesheet\" href=\"{{ asset(\"assets/css/style.css\") }}\">
 
-        {% block javascripts %}
+            <!-- fonts -->
+            <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic|Roboto+Condensed:300italic,400italic,700italic,400,300,700|Oxygen:400,300,700' rel='stylesheet'>
+            <script src=\"https://kit.fontawesome.com/9b628f0366.js\" crossorigin=\"anonymous\"></script>
         {% endblock %}
     </head>
-    <body>
-        {% block body %}{% endblock %}
+        <body id=\"home\">
+        {% block body %}
+            <!-- ****************************** Preloader ************************** -->
+
+            <div id=\"preloader\"></div>
+
+            <!-- ****************************** Sidebar ************************** -->
+
+            <nav id=\"sidebar-wrapper\">
+                <a id=\"menu-close\" href=\"#\" class=\"close-btn toggle\"><i class=\"ion-ios-close-empty\"></i></a>
+                <ul class=\"sidebar-nav\">
+                    <li><a href=\"/user/\"><i class=\"fa-solid fa-right-to-bracket\"></i></a></li>
+                    <li><a href=\"#home\">Home</a></li>
+                    <li><a href=\"#features\">Features</a></li>
+                    <li><a href=\"#gallery\">Gallery</a></li>
+                    <li><a href=\"#team\">Development Team</a></li>
+                    <li><a href=\"#contact\">Contact us</a></li>
+                </ul>
+            </nav>
+
+            <!-- ****************************** Header ************************** -->
+
+            <header class=\"sticky\" id=\"header\">
+                <section class=\"container\">
+                    <section class=\"row\" id=\"logo_menu\">
+                        <section class=\"col-xs-8\"><a class=\"logo\" href=\"\">T R I C O U N T</a></section>
+                        <section class=\"col-xs-4\"><a id=\"menu-toggle\" href=\"#\" class=\"toggle wow rotateIn\" data-wow-delay=\"1s\"><i class=\"ion-navicon\"></i></a></section>
+                    </section>
+                </section>
+            </header>
+
+            <!-- ****************************** Banner ************************** -->
+            <div>
+                {% block content %}
+                    <!-- Contenu spécifique de chaque page -->
+                {% endblock %}
+            </div>
+            
+        {% endblock %}
+        {% block javascripts %}
+            <script src=\"{{ asset('assets/js/jquery-2.1.3.min.js') }}\"></script>
+            <script src=\"{{ asset('assets/js/bootstrap.min.js') }}\"></script>
+            <script src=\"{{ asset('assets/js/wow.min.js') }}\"></script>
+            <script src=\"{{ asset('assets/js/owl.carousel.js') }}\"></script>
+            <script src=\"{{ asset('assets/js/script.js') }}\"></script>
+            
+        {% endblock %}
     </body>
 </html>
 ", "base.html.twig", "/Users/ramjoh/documents_local/Tricount/templates/base.html.twig");
